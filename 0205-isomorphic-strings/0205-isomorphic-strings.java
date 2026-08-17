019cd0efffcce0ca84250a4856e17d78a79bcaf2
Sai Sowjanya Meka <mekasowjanya24@gmail.com>
2026-08-17 23:24:46 +0530

    Time: 6 ms (89.18%), Space: 43.7 MB (86.96%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
+        Arrays.fill(mapST, -1);
+        Arrays.fill(mapTS, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapST[c1] == -1 && mapTS[c2] == -1) {
+                mapST[c1] = c2;
+                mapTS[c2] = c1;
+            } else if (mapST[c1] != c2 || mapTS[c2] != c1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
