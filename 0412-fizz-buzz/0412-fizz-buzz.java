8d82ec616d864c87a636e0c1f77c84c74c434bc3
Sai Sowjanya Meka <mekasowjanya24@gmail.com>
2026-08-25 17:58:46 +0530

    Time: 2 ms (13.67%), Space: 46.7 MB (65.85%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String>answer=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            String s=String.valueOf(i);
+            if(i%3==0 && i%5==0)
+            answer.add("FizzBuzz");
+            else if(i%3==0)
+            answer.add("Fizz");
+            else if(i%5==0)
+            answer.add("Buzz");
+            else
+            answer.add(s);
+        }
+return answer;
+
+        
+    }
+}
