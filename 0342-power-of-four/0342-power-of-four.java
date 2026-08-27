5bb8f4bbc1e9f09e01d8db3102fd5f8e404ad274
Sai Sowjanya Meka <mekasowjanya24@gmail.com>
2026-08-27 19:14:59 +0530

    Time: 1 ms (89.82%), Space: 42.8 MB (15.4%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1)
+        return true;
+        if(n<=0)
+        return false;
+        while(n>4){
+            if(n%4==0)
+            n=n/4;
+            else
+            return false;
+            
+        }
+        return n==4;
+
+        
+    }
+}
